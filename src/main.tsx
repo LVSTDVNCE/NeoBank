1bcc459d7fe47bef53500378785443e1744fe083
Mikhail <mtarantin534@gmail.com>
2024-11-21 19:15:39 +0400

    test commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/global-styles.scss'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter
+			future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+		>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 )
